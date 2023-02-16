654f6e11630dd22f4a1b6c5f39e14b94b9dd7f5e
Diphoko_Tholwana <tholwanadiphoko@gmail.com>
2023-02-16 09:59:25 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - prints "Programming is like building a 
+ * multilingual puzzle, following by a newline."
+ * return 0.
+ */
+
+int main(void)
+
+{
+	put("\"Programming is like building a multilingual puzzle");
+	
+	return (0);
+}
