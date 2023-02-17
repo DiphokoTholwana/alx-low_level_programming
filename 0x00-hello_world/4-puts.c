e65528a95dde9014897dba4cbd03004846cd850c
Diphoko_Tholwana <tholwanadiphoko@gmail.com>
2023-02-16 19:16:16 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
-#include <stdio.h>
+#include <stdio.h>                                
 
-
-/**
- * main - prints "programming is like building a multilingual puzzile, followed by a newline."
+/**                                                * main - prints "programming is like building a multilingual puzzile, followed by a newline."
  * return 0.
- */
-
+ */                                               
 int main(void)
 
 {
-	puts("\"programming is like building multilingual puzzle");
+        puts("\"programming is like building multilingual puzzle");
 
-	return (0);
+        return (0);
 }
