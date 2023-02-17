bc98064c8bc8d2896fd7ed8efea6908e1b61840f
Diphoko_Tholwana <tholwanadiphoko@gmail.com>
2023-02-16 22:21:04 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,14 +2,14 @@
 
 /**
  * main - prints "with proper grammar, but the 
- * outcome is a piece of art," followed by newline
+ * outcome is a piece of art," followed by 
+ * new line.
  * Return: Always 0.
- *
  */
 
 int main(void)
 
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	return (0);i
 }
