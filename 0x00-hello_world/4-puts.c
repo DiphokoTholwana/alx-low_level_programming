8faec7454d5653e441ddcf2a26f6d389cd615daa
Diphoko_Tholwana <tholwanadiphoko@gmail.com>
2023-02-16 19:08:20 -0800

    git co" git co :wq

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints "programming is like building a multilingual puzzile, followed by a newline."
+ * return 0.
+ */
+
+int main(void)
+
+{
+	puts("\"programming is like building multilingual puzzle");
+
+	return (0);
+}
