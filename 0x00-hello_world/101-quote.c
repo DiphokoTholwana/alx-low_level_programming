edf4b5d216bc0992d06d5e53115b2b3bbf2436d7
Diphoko_Tholwana <tholwanadiphoko@gmail.com>
2023-02-17 00:20:08 -0800

    100-intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - prints "and the piece of art is useful"
+ * main - prints "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",followed by new
  *   line, to standard error.
  *   Return: Always 1.
@@ -11,6 +11,6 @@
 int main(void)
 
 {
-	write(2, "and the piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
