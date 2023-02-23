e3e5e02b3a71974dfc904c6c94a26d24293dd046
DiphokoTholwana <tholwanadiphoko@gmail.com>
2023-02-23 04:02:56 -0800

    update

--- main.h
+++ main.h
@@ -4,7 +4,6 @@
 
 #include <stdio.h>
 
-
 void positive_or_negative(int i);
 
 int largest_number(int a, int b, int c);
@@ -12,6 +11,5 @@ int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 
 int convert_day(int month, int day);
-
 
 #endif
