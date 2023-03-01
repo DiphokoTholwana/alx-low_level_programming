e67b95d1faf14b2fc2aa8d9b27bf1ea676c7cc10
DiphokoTholwana <tholwanadiphoko@gmail.com>
2023-03-01 09:57:56 -0800

    update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,11 +15,16 @@ char *_strcat(char *dest, char *src)
 	i = 0;
 	while (dest[i] != '\0')
 	{
-		dest[i] = src[j];
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		desti] =src[j];
 		i++;
 		j++;
 	}
 
-	dest[i] = '\0';
+	dest[i] = \0';
 	return (dest);
 }
